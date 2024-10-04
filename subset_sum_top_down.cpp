8d70ea007da6925fb11130a3f0200459769ecf99
HridaySarder <hridoyaug12@gmail.com>
2024-10-04 19:36:40 +0600

    added memoization

--- subset_sum_top_down.cpp
+++ subset_sum_top_down.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
+int dp[1005][1005];
 bool subset_sum(int n, int a[], int s)
 {
     if (n == 0)
@@ -9,21 +10,23 @@ bool subset_sum(int n, int a[], int s)
         else
             return false;
     }
+    if (dp[n][s] == -1)
+        return dp[n][s];
     if (a[n - 1] <= s)
     {
         bool op1 = subset_sum(n - 1, a, s - a[n - 1]);
         bool op2 = subset_sum(n - 1, a, s);
-        return op1 || op2;
+        return dp[n][s] = op1 || op2;
     }
     else
-        return subset_sum(n - 1, a, s);
+        return dp[n][s] = subset_sum(n - 1, a, s);
 }
 int main()
 {
     int n;
     cin >> n;
     int a[n];
-    for (int i=0; i < n; i++)
+    for (int i = 0; i < n; i++)
     {
         cin >> a[i];
     }
